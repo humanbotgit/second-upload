1262ef9672c012ca6bfed1f4d928418d9ca132be
humanbotgit <jarancibiasedano@gmail.com>
2023-11-08 16:31:09 -0500

    creacion de header, main, footer

---
+++ frontend/src/app/general/main-footer/main-footer.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-main-footer',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './main-footer.component.html',
+  styleUrl: './main-footer.component.css'
+})
+export class MainFooterComponent {
+
+}
